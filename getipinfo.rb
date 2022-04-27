7505e6f13d990af1a7bfdefc29627df3244d05e6
murat.oguz <murat.oguz@turknet.net.tr>
2022-04-27 23:37:11 +0300

    getipinfo 0.1.0 (new formula)

--- getipinfo.rb
+++ getipinfo.rb
@@ -1,5 +1,5 @@
 class Getipinfo < Formula
-  desc "Get ip address with Golang on your computer/system"
+  desc "Get ip address with Golang on your computer/system "
   homepage "https://github.com/masum0813/getipinfo/"
   url "https://github.com/masum0813/getipinfo/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "8a87146b8c597c5e7c2590de7d2c1b7d8843a642832dbc924484b3b4451d3763"
@@ -9,17 +9,10 @@ class Getipinfo < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
-
-    bin.install 'getipinfo'
-    bin.install Dir["lib"]
-    bin.install Dir["files"]
-    prefix.install "README.md"
-    prefix.install "LICENSE"
-
   end
 
   test do
     output = shell_output(bin/"getipinfo version")
-    assert output.include? "getipinfo version 0.0.10"
+    assert output.include? "getipinfo version 0.1.0"
   end
 end
