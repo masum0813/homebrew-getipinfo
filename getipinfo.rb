48343646fe350f33d623a838af0591bfc6fc7d4c
murat.oguz <murat.oguz@turknet.net.tr>
2022-04-27 23:08:14 +0300

    Fixed shasum256

--- getipinfo.rb
+++ getipinfo.rb
@@ -5,7 +5,7 @@ class Getipinfo < Formula
   desc "Get ip address with Golang on your computer/system "
   homepage "https://github.com/masum0813/getipinfo/"
   url "https://github.com/masum0813/getipinfo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f6cb998e3f0ffd0195a4ca6212c9e2269397f8f84b29456d8f5285a04cc907a2"
+  sha256 "8a87146b8c597c5e7c2590de7d2c1b7d8843a642832dbc924484b3b4451d3763"
   license "MIT"
 
   depends_on "go" => :build
